5ab6b04819192507aa7f540d5711893197d20fed
Chris Eskine <chriseskine3@gmail.com>
2018-03-10 18:23:05 -0600

    More servo things DAB

--- src/org/usfirst/frc/team3946/robot/commands/ReadyIntakeArm.java
+++ src/org/usfirst/frc/team3946/robot/commands/ReadyIntakeArm.java
@@ -25,6 +25,7 @@ public class ReadyIntakeArm extends Command {
     protected void execute() {
     	Robot.intake.leftIntakeServo.set(endL);
     	Robot.intake.rightIntakeServo.set(endR);
+    	Robot.intake.leftIntakeServo.setSpeed(.5);
     }
 
     // Make this return true when this Command no longer needs to run execute()
