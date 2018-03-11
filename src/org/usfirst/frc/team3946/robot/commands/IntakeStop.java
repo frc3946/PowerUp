705e11e5bdea094a042513077084fb2bd29fa994
Chris Eskine <chriseskine3@gmail.com>
2018-03-10 18:51:05 -0600

    Servo Stuff unfinished

--- src/org/usfirst/frc/team3946/robot/commands/IntakeStop.java
+++ src/org/usfirst/frc/team3946/robot/commands/IntakeStop.java
@@ -21,7 +21,6 @@ public class IntakeStop extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.intake.intakeStop(0);
     }
 
     // Make this return true when this Command no longer needs to run execute()
